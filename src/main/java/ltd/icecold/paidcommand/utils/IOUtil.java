6edbcf76f83f96dc640fac8c2c9b8b852e038c65
ice <736131306@qq.com>
2020-10-01 18:08:31 +0800

    修复1.7.10无法加载的问题

--- src/main/java/ltd/icecold/paidcommand/utils/IOUtil.java
+++ src/main/java/ltd/icecold/paidcommand/utils/IOUtil.java
@@ -2,7 +2,6 @@ package ltd.icecold.paidcommand.utils;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.stream.Stream;
 
 
 /**
@@ -43,106 +42,4 @@ public class IOUtil {
         }
     }
 
-    /**
-     * @param f    要写入的文件
-     * @param data 数据
-     */
-    public static void writeFile(File f, String data) {
-        writeFile(f, data.getBytes(StandardCharsets.UTF_8));
-    }
-
-    /**
-     * @param f    要写入的文件
-     * @param data 数据
-     */
-    public static void writeFile(File f, byte[] data) {
-        try {
-            if (!f.exists()) {
-                f.createNewFile();
-            }
-            BufferedOutputStream boss = new BufferedOutputStream(new FileOutputStream(f));
-            boss.write(data);
-            boss.flush();
-            boss.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * @param sourcePath 原路径
-     * @param newPath    新路径
-     */
-    public static void copyDir(String sourcePath, String newPath) {
-        File start = new File(sourcePath);
-        File end = new File(newPath);
-        copyDir(start, end);
-    }
-
-    public static void copyDir(File start, File end) {
-        String[] filePath = start.list();
-        if (!end.exists()) {
-            end.mkdir();
-        }
-
-        Stream.of(filePath).forEach((v) -> {
-            File f = new File(start, v);
-            if (f.isDirectory()) {
-                copyDir(f, new File(end, v));
-            } else {
-                copyFile(new File(start, v), new File(end, v));
-            }
-        });
-    }
-
-    /**
-     * @param sourcePath 原路径
-     * @param newPath    新路径
-     */
-    public static void copyFile(String sourcePath, String newPath) {
-        File start = new File(sourcePath);
-        File end = new File(newPath);
-        copyFile(start, end);
-    }
-
-    public static void copyFile(File start, File end) {
-        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(start));
-             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(end))) {
-            byte[] b = new byte[bis.available()];
-            bis.read(b);
-            bos.write(b);
-            bos.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 删除路径
-     *
-     * @param path 欲删除路径
-     * @return 是否成功删除
-     */
-    public static boolean deletePath(String path) {
-        File file = new File(path);
-        if (!file.exists()) {
-            return false;
-        }
-        if (file.isFile()) {
-            return file.delete();
-        }
-        File[] files = file.listFiles();
-        for (File f : files) {
-            if (f.isFile()) {
-                if (!f.delete()) {
-                    return false;
-                }
-            } else {
-                if (!deletePath(f.getAbsolutePath())) {
-                    return false;
-                }
-            }
-        }
-        return file.delete();
-    }
 }
